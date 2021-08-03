61d3386f2fd77e9fdd6911626d1e573a66ce9235
DimboScript <yacsarblya@gmail.com>
2021-08-03 10:44:29 +0600

    add switch construction

--- study/BeOnMax/ifElseSwitch.js
+++ study/BeOnMax/ifElseSwitch.js
@@ -2,10 +2,24 @@
 
 let number = Math.floor(Math.random() * (150 - 10)) + 10;
 console.log(number);
+let num = 50;
+
 if (number < 50) {
     console.log('Меньше 50');
 } else if (number > 50) {
     console.log('Больше 50');
 } else {
     console.log('Что то пошло не так');
+}
+
+switch (num) {
+    case 50:
+        console.log('Больше 50');
+        break;
+    case 40:
+        console.log('Меньше 50');
+        break;
+    default:
+        console.log('Что то пошло не так');
+        break;
 }
